e41158494c05bfef9eb1dc56d19307bc38a02087
Miguel Ángel Alba Blanco <miguelangel.abb@gmail.com>
2019-02-13 22:48:58 +0100

    Modifico datos
    El problema del código estaba en que teía que pasabr a escala de grises la imagen

--- 2018-2019/ejercicios_propuestos_clase/Filtrado_dominio_frecuencia/Ejerciciosfiltradofrecuencia.m
+++ 2018-2019/ejercicios_propuestos_clase/Filtrado_dominio_frecuencia/Ejerciciosfiltradofrecuencia.m
@@ -6,17 +6,17 @@ FFT_images = [];
 modulo_FFT = [];
 modulo_FF_contraste = [];
 for i= 1:6
-    imagenes{i} = imread(lista(i).name);
+    imagenes{i} = rgb2gray(imread(lista(i).name));
     m = size(imagenes{i},1);
     n = size(imagenes{i},2)
     FFT_images{i} = fftshift(fft2(double(imagenes{i}),m,n));
     modulo_FFT{i} = abs(FFT_images{i});
-     for j=1:m
-        modulo_FF_contraste{i}(j,:)= log(1 + abs(modulo_FFT{i}(j,:))); %% Paso a logratimo para ver mejor
-    end;
+    % for j=1:m
+     %   modulo_FF_contraste{i}(j,:)= log(1 + abs(modulo_FFT{i}(j,:))); %% Paso a logratimo para ver mejor
+    %end;
     figure,
     subplot(2,1,1), imshow(imagenes{i}),title("Imagen Ejercicio "+i);
-    subplot(2,1,2), imshow(modulo_FF_contraste{i}, []), title("Módulo FFT de Ejercicio "+i);
+    subplot(2,1,2), imshow(modulo_FFT{i}, []), title("Módulo FFT de Ejercicio "+i);
 end 
 
 
@@ -24,10 +24,11 @@ end
   clear all, close all, clc
 
     imagen = imread('Ejercicio5.PNG');
-    figure, imshow(imagen);
-    m = size(imagen,1);
-    n = size(imagen,2)
-    FFT_image = fftshift(fft2(double(imagen),m,n));
+    EJ1 = rgb2gray(imagen);
+    figure, imshow(EJ1);
+    m = size(EJ1,1);
+    n = size(EJ1,2)
+    FFT_image = fftshift(fft2(double(EJ1),m,n));
     modulo_FF = abs(FFT_image);
     for i=1:m
         modulo_FF_contraste(i,:)= log(1 + abs(modulo_FF(i,:)));
